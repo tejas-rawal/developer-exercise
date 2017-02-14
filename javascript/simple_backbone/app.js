8743d838596e6eaa57ba7a83afdc7117772d4ecf
Tejas Rawal <trawal90@gmail.com>
2017-02-14 12:18:40 -0600

    Refactor QuoteListView render function. Fetch collection on initialize

--- javascript/simple_backbone/app.js
+++ javascript/simple_backbone/app.js
@@ -15,7 +15,11 @@ app.Quote = Backbone.Model.extend({
 // Collection
 app.QuoteList = Backbone.Collection.extend({
 	model: app.Quote,
-	url: 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json'
+	url: 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json',
+
+	initialize: function(fetchOptions) {
+		this.fetch(fetchOptions);
+	}
 });
 
 //  Single quote object view
@@ -24,7 +28,7 @@ app.QuoteItemView = Backbone.View.extend({
 	template: _.template($('#item-template').html()),
 
 	render: function() {
-		$(this.el).html(this.template(this.model));
+		$(this.el).html(this.template(this.model.toJSON()));
 		return this;
 	}
 });
@@ -34,11 +38,15 @@ app.QuoteListView = Backbone.View.extend({
 	el: '#quotes-list',
 	tagName: 'tbody',
 
+	initialize: function() {
+		_.bindAll(this, 'render', 'renderItem');
+		this.render();
+	},
+
 	render: function() {
 		this.$el.empty();
-
-		_.each(this.collection, function(quoteObject){
-			this.renderItem(quoteObject);
+		_.each(this.collection.models, function(item){
+			this.renderItem(item);
 		}, this);
 	},
 
